74f80da9faa0ac0485e20e78a43b03f2173d4ac2
VGrigoriev <vgrigoriev@ptsecurity.com>
2020-05-10 02:17:18 +0000

    Fix code style

--- common/Memory.hpp
+++ common/Memory.hpp
@@ -16,7 +16,7 @@ namespace pdl::common::memory
     /**
       * @brief Function that allocates and constructs object.
       *
-      * @tparam [in] Type - Type of allocated the object.
+      * @tparam [in] Type - Type of object.
       * @tparam [in] args - Arguments for construct the object.
       * @return Smart pointer to allocated object.
       */
@@ -29,29 +29,31 @@ namespace pdl::common::memory
     /**
       * @brief Function that allocates array of selected type and if needed fills it.
       *
-      * @tparam [in] Type   - Type of allocated data.
-      * @param  [in] count  - Number of elements in array.
-      * @param  [in] data   - Pointer to any copied data.   Default: nullptr.
-      * @param  [in] length - Size of copied data in bytes. Default: 0.
+      * @tparam [in] Type  - Type of data.
+      * @param  [in] count - Number of elements in array.
+      * @param  [in] data  - Pointer to any copied data.   Default: nullptr.
+      * @param  [in] size  - Size of copied data in bytes. Default: 0.
       * @return Smart pointer to allocated array.
       */
     template <typename Type>
     std::unique_ptr<Type[]> allocArray (const std::size_t count,
-                                        const void* const data = nullptr,
-                                        const std::size_t length = 0)
+                                        const void * const data = nullptr,
+                                        const std::size_t size = 0)
     {
         assert(count > 0);
 
         auto memory = std::make_unique<Type[]>(count);
-        if (data == nullptr || length == 0) { return memory; }
-
-        const std::size_t allocatedBytes = count * sizeof(Type);
-        if (allocatedBytes <= length) {
-            memcpy(memory.get(), data, allocatedBytes);
+        if (!data || size == 0) {
+            return memory;
         }
-        else {  // If copied data length less then allocated data size.
-            memcpy(memory.get(), data, length);
-            memset(memory.get() + length, 0, allocatedBytes - length);
+
+        const std::size_t bytes = count * sizeof(Type);
+        if (bytes <= size) {
+            memcpy(memory.get(), data, bytes);
+        }
+        else {  // If copied data size less then allocated data size.
+            memcpy(memory.get(), data, size);
+            memset(memory.get() + size, 0, bytes - size);
         }
         return memory;
     }
@@ -59,10 +61,10 @@ namespace pdl::common::memory
     /**
       * @brief Function that allocates and constructs array of object.
       *
-      * @tparam [in] Type  - Type of allocated data.
+      * @tparam [in] Type  - Type of object.
       * @param  [in] count - Number of elements in array.
-      * @tparam [in] args  - Arguments for construct object in array.
-      * @return Smart pointer to allocated array of objects.
+      * @tparam [in] args  - Arguments for construct each object in array.
+      * @return Smart pointer to allocated array.
       */
     template <typename Type, typename... Args>
     std::unique_ptr<std::unique_ptr<Type>[]> allocArrayOfObjects (const std::size_t count, const Args&... args)
