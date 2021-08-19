f76d229d25c760b5d7908539c5302d2c4507f18f
Vitaly Grigoriev <Vit.link420@gmail.com>
2021-08-19 13:11:10 +0300

    Add Coverity step to GitHub Actions

--- coverity_scan.sh
+++ coverity_scan.sh
@@ -6,19 +6,20 @@ then
  exit "0"
 fi
 
-export CXX="/usr/bin/g++"
+export CXX="/usr/bin/g++-9"
 export BUILD_TYPE="Debug"
 
-mkdir check && cd check && cmake -DPDL_WITH_TESTS=1 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_CXX_COMPILER=${CXX} -j4 ..
+mkdir check && cd check
 
-/home/vitaly/Coverity/bin/cov-configure --comptype gcc --compiler gcc-9 --template
-/home/vitaly/Coverity/bin/cov-build --dir cov-int make -j4 && tar czvf project.tgz cov-int && curl \
-  --form token=2d9uWgkZDh2Nms5m4tp3sg \
-  --form email=Vit.link420@gmail.com \
-  --form file=@project.tgz \
-  --form version="$1" \
-  --form description="$2" \
-  https://scan.coverity.com/builds?project=Vitaliy-Grigoriev%2FPDL && echo "Send packet success."
+/usr/coverity/bin/cov-configure --comptype g++ --compiler g++-9 --template
+cmake -DPDL_WITH_TESTS=1 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_CXX_COMPILER=${CXX} -j4 ..
+/usr/coverity/bin/cov-build --dir cov-int make -j4 && tar czvf project.tgz cov-int
+curl --form token="${COVERITY_SCAN_TOKEN}" \
+     --form email=Vit.link420@gmail.com    \
+     --form file=@project.tgz              \
+     --form version="$1"                   \
+     --form description="$2"               \
+     https://scan.coverity.com/builds?project=Vitaliy-Grigoriev%2FPDL
 
 cp cov-int/build-log.txt ../coverity-build-log.txt
 cd .. && rm -rf check
